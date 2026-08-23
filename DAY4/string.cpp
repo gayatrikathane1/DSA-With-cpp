534264eea09e498d98572dbd7c78f38d336aa954
gayatrikathane <gayatrikathane17@gmail.com>
2026-08-23 07:29:24 +0530

    DAY4

---
+++ DAY4/string.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str = "programming";
+    // cout<<str.append("World");
+    // cout<<str.substr(0,3);
+    cout<<str.find("a");
+    return 0;
+}
